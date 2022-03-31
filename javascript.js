ec90c58677302731fc944a304385f69026f87877
Cumali Atalan <cumaliatalan97@gmail.com>
2022-03-31 22:38:01 +0300

    add logos

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function showMyCV(){
+    document.getElementsByClassName("cv-button")
+    console.log("CV Göster")
+}
+
+function myFunction() {
+    document.body.style.backgroundColor="Black";
+}
+
+document.addEventListener('click', myFunction)
+document.addEventListener('click', showMyCV)
